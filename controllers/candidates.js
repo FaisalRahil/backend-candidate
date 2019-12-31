270fa32245bafa1980b14b02762eef3f14319832
Faisal <faisallrahil@gmail.com>
2019-12-31 20:39:32 +0200

    add geocoder middleware packeg on utile
    add geocoder middleware packeg on utile

--- controllers/candidates.js
+++ controllers/candidates.js
@@ -1,113 +1,92 @@
 const Candidate = require("../models/Candidate");
 const ErrorResponse = require("../utils/errorResponse");
+const asyncHandler = require("../middleware/async");
 
 // @desc      Get all candidates
 // @route     GET /api/v1/candidates
 // @access    Public
-exports.getCandidates = async (req, res, next) => {
-    try {
-        const candidates = await Candidate.find();
-
-        res.status(200).json({
-            success: true,
-            count: candidates.length,
-            data: candidates
-        });
-    } catch (err) {
-        next(err);
-    }
-};
+exports.getCandidates = asyncHandler(async (req, res, next) => {
+    const candidates = await Candidate.find();
+    res.status(200).json({
+        success: true,
+        count: candidates.length,
+        data: candidates
+    });
+});
 
 // @desc      Get all candidate
 // @route     GET /api/v1/candidates/:id
 // @access    Public
-exports.getCandidate = async (req, res, next) => {
-    try {
-        const candidate = await Candidate.findById(req.params.id);
+exports.getCandidate = asyncHandler(async (req, res, next) => {
+    const candidate = await Candidate.findById(req.params.id);
 
-        if (!candidate) {
-            return next(
-                new ErrorResponse(
-                    `Candidate not found with id of ${req.params.id}`,
-                    404
-                )
-            );
-        }
-
-        res.status(200).json({
-            success: true,
-            data: candidate
-        });
-    } catch (err) {
-        // res.status(400).json({ success: false });
-        next(err);
+    if (!candidate) {
+        return next(
+            new ErrorResponse(
+                `Candidate not found with id of ${req.params.id}`,
+                404
+            )
+        );
     }
-};
+
+    res.status(200).json({
+        success: true,
+        data: candidate
+    });
+});
 
 // @desc      Create new candidate
 // @route     POST /api/v1/candidates
 // @access    Privete
-exports.createCandidate = async (req, res, next) => {
-    try {
-        const candidate = await Candidate.create(req.body);
-        res.status(201).json({
-            success: true,
-            data: candidate
-        });
-    } catch (err) {
-        next(err);
-    }
-};
+exports.createCandidate = asyncHandler(async (req, res, next) => {
+    const candidate = await Candidate.create(req.body);
+    res.status(201).json({
+        success: true,
+        data: candidate
+    });
+});
 
 // @desc      Update candidate
 // @route     PUT /api/v1/candidates/:id
 // @access    Privete
-exports.updateCandidate = async (req, res, next) => {
-    try {
-        const candidate = await Candidate.findByIdAndUpdate(
-            req.params.id,
-            req.body,
-            {
-                new: true,
-                runValidators: true
-            }
-        );
-        if (!candidate) {
-            return next(
-                new ErrorResponse(
-                    `Candidate not found with id of ${req.params.id}`,
-                    404
-                )
-            );
+exports.updateCandidate = asyncHandler(async (req, res, next) => {
+    const candidate = await Candidate.findByIdAndUpdate(
+        req.params.id,
+        req.body,
+        {
+            new: true,
+            runValidators: true
         }
-        res.status(200).json({
-            success: true,
-            data: candidate
-        });
-    } catch (err) {
-        next(err);
+    );
+    if (!candidate) {
+        return next(
+            new ErrorResponse(
+                `Candidate not found with id of ${req.params.id}`,
+                404
+            )
+        );
     }
-};
+    res.status(200).json({
+        success: true,
+        data: candidate
+    });
+});
 
 // @desc      Delete candidate
 // @route     DELETE /api/v1/candidates/:id
 // @access    Privete
-exports.deleteCandidate = async (req, res, next) => {
-    try {
-        const candidate = await Candidate.findByIdAndDelete(req.params.id);
-        if (!candidate) {
-            return next(
-                new ErrorResponse(
-                    `Candidate not found with id of ${req.params.id}`,
-                    404
-                )
-            );
-        }
-        res.status(200).json({
-            success: true,
-            data: {}
-        });
-    } catch (err) {
-        next(err);
+exports.deleteCandidate = asyncHandler(async (req, res, next) => {
+    const candidate = await Candidate.findByIdAndDelete(req.params.id);
+    if (!candidate) {
+        return next(
+            new ErrorResponse(
+                `Candidate not found with id of ${req.params.id}`,
+                404
+            )
+        );
     }
-};
+    res.status(200).json({
+        success: true,
+        data: {}
+    });
+});
--- middleware/error.js
+++ middleware/error.js
@@ -1,7 +1,7 @@
 const ErrorResponse = require("../utils/errorResponse");
 const errorHandler = (err, req, res, next) => {
     // log to consol for dev
-    console.log(err.stack.red);
+    console.log(err);
     let error = { ...err };
 
     error.message = err.message;
@@ -10,6 +10,18 @@ const errorHandler = (err, req, res, next) => {
     if (err.name === "CastError") {
         const message = `Resource not found ${err.value}`;
         error = new ErrorResponse(message, 404);
+    }
+
+    // Mongoose duplicate key
+    if (err.code === 11000) {
+        const message = "Duplicate field value entered";
+        error = new ErrorResponse(message, 400);
+    }
+
+    // Mongoose validation error
+    if (err.name === "ValidationError") {
+        const message = Object.values(err.errors).map(val => val.message);
+        error = new ErrorResponse(message, 400);
     }
 
     res.status(error.statusCode || 500).json({
