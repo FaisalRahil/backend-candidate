f5a1ddeb812707aa2f2199f34260f27116331a82
shafikaradi <shafik93@icloud.com>
2020-01-08 00:43:13 +0200

    more of test functions are added

--- models/Candidate.js
+++ models/Candidate.js
@@ -107,22 +107,22 @@ CandidateSchema.pre("save", function(next) {
 });
 
 // Geocode & create location field
-CandidateSchema.pre("save", async function(next) {
-    const loc = await geocoder.geocode(this.address);
-    this.location = {
-        ype: "Point",
-        coordinates: [loc[0].longitude, loc[0].latitude],
-        formattedAddress: loc[0].formattedAddress,
-        street: loc[0].streetName,
-        city: loc[0].city,
-        state: loc[0].stateCode,
-        zipcode: loc[0].zipcode,
-        country: loc[0].countryCode
-    };
+// CandidateSchema.pre("save", async function(next) {
+//     const loc = await geocoder.geocode(this.address);
+//     this.location = {
+//         ype: "Point",
+//         coordinates: [loc[0].longitude, loc[0].latitude],
+//         formattedAddress: loc[0].formattedAddress,
+//         street: loc[0].streetName,
+//         city: loc[0].city,
+//         state: loc[0].stateCode,
+//         zipcode: loc[0].zipcode,
+//         country: loc[0].countryCode
+//     };
 
-    // Do not save address in DB
-    this.address = undefined;
-    next();
-});
+//     // Do not save address in DB
+//     this.address = undefined;
+//     next();
+// });
 
 module.exports = mongoose.model("Candidate", CandidateSchema);
--- tests/unit/routes/candidate.test.js
+++ tests/unit/routes/candidate.test.js
@@ -9,22 +9,96 @@ app.use(candidates);
 describe('candiate route test', ()=> {
     it('should create a candidate', () => {
      
-        
-    })
+        let candidate = {
+            name:"shafiq",
+            slug:"aaaaaa",
+            description:"Candidate",
+            website:"https://www.google.com",
+            phone:"2188287623",
+            email:"s@lc.com",
+            address:"hay anadlus",
+            location:{
+                type:"Point",
+                coordinates:[13.234,32.2334],
+                formattedAddress:"hay anadlus",
+                street:"Bashir assedwie street",
+                city: "tripoli",
+                state: "tripoli",
+                zipcode: "234232",
+                country: "Libya"
+                
+            },
+            careers:"Mobile Development",
+            averageRating:5,
+            averageCost:250000000
+            
+       }
 
-    it('should get exisiting candidates', () => {
+       
+
+       request(app)
+       .post('/')
+       .set({'Content-Type':'application/json'})
+       .send(candidate)
+       .expect('Content-Type', 'application/json; charset=utf-8')
+       .expect(201)
+       .end(function(err, res) {
+           if (err) throw err;
+        
+         
+         });
      
        
     })
 
+
+    it('should get exisiting candidates', () => {
+
+        request(app)
+        .get('/')
+        .expect(200)
+        .end(function(err, res) {
+            if (err) throw err;
+
+            assert.equal(res.body.data,0)
+         
+            console.error(res)
+          });
+      
+    })
+
     it('should get an exisiting candidate', () => {
-     
+        request(app)
+        .get('/5e14de69d46e597f76712b76')
+        .expect(200)
+        .end(function(err, res) {
+            if (err) throw err;
+
+          
+         
+            console.error(res)
+          });
+       
        
     })
 
     it('should update an exisiting candidate', () => {
-     
-       
+    
+    })
+
+    it('should send an error on upding non-existing candidate', () => {
+        
+        request(app)
+        .put('/5e14de69d46e597f76712b78')
+        .expect(404)
+        .end(function(err, res) {
+            if (err) throw err;
+
+            assert.equal(res.body.error,"Candidate not found with id of 5e14de69d46e597f76712b78")
+         
+            console.error(res)
+          });
+      
     })
 
     it('should delete an exisiting candidate', () => {
@@ -54,12 +128,11 @@ describe('candiate route test', ()=> {
      
         request(app)
         .get('/parm/bye')
-        
         .expect('Content-Type', 'application/json; charset=utf-8')
         .expect(200)
         .end(function(err, res) {
             if (err) throw err;
-            console.error(res.text)
+           
           });
     })
 
