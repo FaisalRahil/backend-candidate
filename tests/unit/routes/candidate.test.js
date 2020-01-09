7080e4f09eb6635b63823a735c96e9162fa85c90
shafikaradi <shafik93@icloud.com>
2020-01-09 02:40:21 +0200

    candidate model tests are added and also other tests are added to routes/candidate

--- tests/unit/routes/candidate.test.js
+++ tests/unit/routes/candidate.test.js
@@ -1,6 +1,7 @@
 const assert = require('assert')
 const request = require('supertest');
 const express = require("express");
+const chai = require('chai')
 const app = express()
 const candidates = require('../../../routes/candidates')
 
@@ -9,18 +10,18 @@ app.use(candidates);
 describe('candiate route test', ()=> {
     it('should create a candidate', () => {
      
-        let candidate = {
+        const candidate = {
             name:"shafiq",
             slug:"aaaaaa",
             description:"Candidate",
             website:"https://www.google.com",
             phone:"2188287623",
             email:"s@lc.com",
-            address:"hay anadlus",
+            address:"hay andalus",
             location:{
                 type:"Point",
                 coordinates:[13.234,32.2334],
-                formattedAddress:"hay anadlus",
+                formattedAddress:"hay andalus",
                 street:"Bashir assedwie street",
                 city: "tripoli",
                 state: "tripoli",
@@ -69,7 +70,7 @@ describe('candiate route test', ()=> {
 
     it('should get an exisiting candidate', () => {
         request(app)
-        .get('/5e14de69d46e597f76712b76')
+        .get('/5e166893db837a963d999160')
         .expect(200)
         .end(function(err, res) {
             if (err) throw err;
@@ -83,14 +84,80 @@ describe('candiate route test', ()=> {
     })
 
     it('should update an exisiting candidate', () => {
+
+        const candidateUpdatedData = {
+            name:"shafiq K. El aradi",
+            slug:"xmrmxmrxmr",
+            description:"Candidate of Tripoli Centeral",
+            website:"https://www.google.com",
+            phone:"2188287623",
+            email:"s@lc.com",
+            address:"Tripoli Centeral",
+            location:{
+                type:"Point",
+                coordinates:[32.343,13.45234],
+                formattedAddress:"Tripoli Centeral",
+                street:"Bashir assedwie street",
+                city: "tripoli",
+                state: "tripoli",
+                zipcode: "234232",
+                country: "Libya"
+                
+            },
+            careers:"Other",
+            averageRating:10,
+            averageCost:260000000
+            
+       }
+
+        request(app)
+        .put('/5e166893db837a963d999160')
+        .expect(200)
+        .send(candidateUpdatedData)
+        .end(function(err, res) {
+            if (err) throw err;
+
+            
+
+             assert.ok(res.status)
+             chai.assert.isNotNull(res.data)
+         
+            console.error(res)
+          });
     
     })
 
     it('should send an error on upding non-existing candidate', () => {
         
+        const candidateUpdatedData = {
+            name:"shafiq K. El aradi",
+            slug:"xmrmxmrxmr",
+            description:"Candidate of Tripoli Centeral",
+            website:"https://www.google.com",
+            phone:"2188287623",
+            email:"s@lc.com",
+            address:"Tripoli Centeral",
+            location:{
+                type:"Point",
+                coordinates:[32.343,13.45234],
+                formattedAddress:"Tripoli Centeral",
+                street:"Bashir assedwie street",
+                city: "tripoli",
+                state: "tripoli",
+                zipcode: "234232",
+                country: "Libya"
+                
+            },
+            careers:"Other",
+            averageRating:10,
+            averageCost:260000000
+            
+       }
+
         request(app)
         .put('/5e14de69d46e597f76712b78')
-        .expect(404)
+        .send(candidateUpdatedData)
+        .expect(200)
         .end(function(err, res) {
             if (err) throw err;
 
@@ -101,16 +168,39 @@ describe('candiate route test', ()=> {
       
     })
 
-    it('should delete an exisiting candidate', () => {
-     
-       
-    })
+   
 
     it('should delete an exisiting candidate', () => {
      
+        request(app)
+        .delete(`/5e166893db837a963d999160`)
+        .expect(200)
+        .end(function(err, res) {
+            if (err) throw err;
+
+            chai.assert.deepEqual(res.body.data,{})
+            chai.assert.isObject(res.body.data)
+            chai.assert.isTrue(res.body.success)
+          });
        
     })
 
+    it('should send an error on trying deleting non-existing candidate', () => {
+     
+        let id = "5e14de69d46e597f76712b78";
+
+        request(app)
+        .delete(`/${id}`)
+        .expect(404)
+        .end(function(err, res) {
+            if (err) throw err;
+
+            chai.assert.equal(res.body.error,`Candidate not found with id of ${id}`)
+            chai.assert.isFalse(res.body.success)
+          });
+
+       
+    })
 
     it('should request hi routes', () => {
      
@@ -132,6 +222,7 @@ describe('candiate route test', ()=> {
         .expect(200)
         .end(function(err, res) {
             if (err) throw err;
+            
            
           });
     })
